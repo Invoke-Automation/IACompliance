190c987c7f0816d633a5e593b1b0c16cfe6006d7
Tomas Deceuninck <tomas.deceuninck@lidl.be>
2018-01-30 16:41:35 +0100

    Added Class tests

---
+++ Tests/Classes.Tests.ps1
@@ -0,0 +1,21 @@
+. $PSScriptRoot\_InitializeTests.ps1
+
+. $PSScriptRoot\..\IACompliance\Private\_Classes.ps1
+
+Describe 'IAComplianceRule' {
+	It 'Should have a constructor with Name,PreCheck,CheckScript' {
+		{[IAComplianceRule]::New($TestRule1Name,$TestRule1PreCheck,$TestRule1CheckScript)} | Should -Not -Throw
+	}
+	It 'Should have a constructor with Name,CheckScript' {
+		{[IAComplianceRule]::New($TestRule1Name,$TestRule1CheckScript)} | Should -Not -Throw
+	}
+}
+
+Describe 'IAComplianceCheck' {
+}
+
+Describe 'IAComplianceRuleCheckResult' {
+}
+
+Describe 'IAComplianceReport' {
+}
