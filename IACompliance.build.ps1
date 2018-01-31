3332a769f0e7e891be397b5d98ca3ad33c806243
Tomas Deceuninck <tomas.deceuninck@gmail.com>
2018-01-31 22:22:14 +0100

    Cleanup/rework Appveyor CI

--- IACompliance.build.ps1
+++ IACompliance.build.ps1
@@ -10,11 +10,18 @@ $DocsLocale = 'en-US'
 $ModuleManifestPath = Join-Path $ModulePath "$ModuleName.psd1"
 $LocalBuildPath = Join-Path $RepoRoot 'build'
 
-task Tests {
-    $result = Invoke-Pester -PassThru
-
-    if($result.FailedCount -gt 0){
-        throw 'Some Tests failed'
+task PesterTests {
+    try {
+        $testResultsFile = "$RepoRoot\TestResult.xml"
+        $result = Invoke-Pester -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+        if ($env:APPVEYOR) {
+            (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+        }
+        Remove-Item $testResultsFile -Force
+        assert ($result.FailedCount -eq 0) "$($result.FailedCount) Pester test(s) failed."
+    }
+    catch {
+        throw
     }
 }
 
@@ -37,7 +44,7 @@ task Docs {
 	}
 }
 
-task AppVoyerBuild -If ($env:APPVEYOR) Docs, {
+task AppVeyorBuild -If ($env:APPVEYOR) Docs, {
     # Make sure we're using the Master branch and that it's not a pull request
     # Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
     if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
@@ -48,16 +55,9 @@ task AppVoyerBuild -If ($env:APPVEYOR) Docs, {
         # We're going to add 1 to the revision value since a new commit has been merged to Master
         # This means that the major / minor / build values will be consistent across GitHub and the Gallery
         Try {
-            # Start by importing the manifest to determine the version, then add 1 to the revision
-            $manifest = Test-ModuleManifest -Path $ModuleManifestPath
-            [System.Version]$version = $manifest.Version
-            Write-Output "Old Version: $version"
-            [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, ($version.Build + 1), 0)
-            Write-Output "New Version: $newVersion"
-    
             # Update the manifest with the new version value and fix the weird string replace bug
             $functionList = ((Get-ChildItem -Path $PublicFunctionsPath -Recurse -Filter "*.ps1").BaseName)
-            Update-ModuleManifest -Path $ModuleManifestPath -ModuleVersion $newVersion -FunctionsToExport $functionList
+            Update-ModuleManifest -Path $ModuleManifestPath -ModuleVersion [version]$env:APPVEYOR_BUILD_VERSION -FunctionsToExport $functionList
             (Get-Content -Path $ModuleManifestPath) -replace "PSGet_$ModuleName", "$ModuleName" | Set-Content -Path $ModuleManifestPath
             (Get-Content -Path $ModuleManifestPath) -replace 'NewManifest', "$ModuleName" | Set-Content -Path $ModuleManifestPath
             (Get-Content -Path $ModuleManifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $ModuleManifestPath -Force
@@ -65,33 +65,37 @@ task AppVoyerBuild -If ($env:APPVEYOR) Docs, {
         } catch {
             throw $_
         }
+    }
+}
 
-        # Publish Module to PSGallery
-        Try {
-            Publish-Module -Path $ModulePath -NuGetApiKey $env:NuGetApiKey -ErrorAction 'Stop'
-            Write-Host "IAUtility PowerShell Module version $newVersion published to the PowerShell Gallery." -ForegroundColor Cyan
-        } Catch {
-            Write-Warning "Publishing update $newVersion to the PowerShell Gallery failed."
-            throw $_
-        }
+task AppVeyorPublish -If ($env:APPVEYOR) {
+    # Publish Module to PSGallery
+    Try {
+        Publish-Module -Path $ModulePath -NuGetApiKey $env:NuGetApiKey -ErrorAction 'Stop'
+        Write-Host "$ModuleName PowerShell Module version $env:APPVEYOR_BUILD_VERSION published to the PowerShell Gallery." -ForegroundColor Cyan
+    } Catch {
+        Write-Warning "Publishing update $env:APPVEYOR_BUILD_VERSION to the PowerShell Gallery failed."
+        throw $_
+    }
 
-        # Publish the new version back to Master on GitHub
-        Try {
-            # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
-            # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
-            $env:Path += ";$env:ProgramFiles\Git\cmd"
-            Import-Module posh-git -ErrorAction Stop
-            git checkout master
-            git add --all
-            git status
-            git commit -s -m "Update version to $newVersion"
-            git push origin master
-            Write-Host "IAUtility PowerShell Module version $newVersion published to GitHub." -ForegroundColor Cyan
-        } Catch {
-            # Sad panda; it broke
-            Write-Warning "Publishing update $newVersion to GitHub failed."
-            throw $_
-        }
+    # Publish the new version back to Master on GitHub
+    Try {
+        # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
+        # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
+        $env:Path += ";$env:ProgramFiles\Git\cmd"
+        Import-Module posh-git -ErrorAction Stop
+        git config --global credential.helper store
+        Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitHubKey):x-oauth-basic@github.com`n"
+        git checkout master
+        git add --all
+        git status
+        git commit -s -m "Update version to $env:APPVEYOR_BUILD_VERSION"
+        git push origin master
+        Write-Host "$ModuleName PowerShell Module version $env:APPVEYOR_BUILD_VERSION published to GitHub." -ForegroundColor Cyan
+    } Catch {
+        # Sad panda; it broke
+        Write-Warning "Publishing update $env:APPVEYOR_BUILD_VERSION to GitHub failed."
+        throw $_
     }
 }
 
@@ -118,4 +122,4 @@ task LocalBuild -If (!$env:APPVEYOR) Docs, {
     }
 }
 
-task . Tests, AppVoyerBuild, LocalBuild
+task . PesterTests, LocalBuild
